b4fe973a1b823ee6657b24f791c160675e2c6c66
Tahsin Nazmee <tnazmee@yahoo.com>
2019-10-30 23:49:51 -0500

    just finish, still need to style

---
+++ Clicky-React/src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Header.css";
+
+const Header = props => (
+  <div className="header">
+    <div className="title">{props.children}</div>
+    <div className="scores">
+      Current Score: {props.score} Top Score: {props.highscore}
+    </div>
+  </div>
+);
+
+export default Header;
