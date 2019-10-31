24553fdac2a71a296064220e3a603246316efd56
Tahsin Nazmee <tnazmee@yahoo.com>
2019-10-31 00:04:33 -0500

    Updates

---
+++ precache-manifest.888f5e1f7f644ed6b918e2b2f824b5b0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abe727e2e2e5e5af4ce1a89bea628479",
+    "url": "/Clickify/index.html"
+  },
+  {
+    "revision": "866532c6a119f6e35fc5",
+    "url": "/Clickify/static/css/main.d228fd16.chunk.css"
+  },
+  {
+    "revision": "4fb6c9bea8f558224c16",
+    "url": "/Clickify/static/js/2.b7a6145f.chunk.js"
+  },
+  {
+    "revision": "866532c6a119f6e35fc5",
+    "url": "/Clickify/static/js/main.30670081.chunk.js"
+  },
+  {
+    "revision": "ba95fb20214083edd4c9",
+    "url": "/Clickify/static/js/runtime-main.4eaf2495.js"
+  }
+]);
